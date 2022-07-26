d49a77d74f2309e6f229958c64a9e9c245321d3b
Snoword <1430750200@qq.com>
2022-07-26 13:27:16 +0800

    fix: bugs

--- components/FolderListLayout.tsx
+++ components/FolderListLayout.tsx
@@ -40,6 +40,7 @@ const FolderListLayout = ({
   toggleTotalSelected,
   totalGenerating,
   handleSelectedDownload,
+  handleSelectedView,
   folderGenerating,
   handleFolderDownload,
   toast,
@@ -78,6 +79,7 @@ const FolderListLayout = ({
             {totalGenerating ? (
               <Downloading title={t('Downloading selected files, refresh page to cancel')} style="p-1.5" />
             ) : (
+              <div className='inline-block'>
               <button
                 title={t('Download selected files')}
                 className="cursor-pointer rounded p-1.5 hover:bg-gray-300 disabled:cursor-not-allowed disabled:text-gray-400 disabled:hover:bg-white dark:hover:bg-gray-600 disabled:dark:text-gray-600 disabled:hover:dark:bg-gray-900"
@@ -86,6 +88,15 @@ const FolderListLayout = ({
               >
                 <FontAwesomeIcon icon={['far', 'arrow-alt-circle-down']} size="lg" />
               </button>
+              <button
+                title={t('View selected files')}
+                className="cursor-pointer rounded p-1.5 hover:bg-gray-300 disabled:cursor-not-allowed disabled:text-gray-400 disabled:hover:bg-white dark:hover:bg-gray-600 disabled:dark:text-gray-600 disabled:hover:dark:bg-gray-900"
+                disabled={totalSelected === 0}
+                onClick={handleSelectedView}
+              >
+                <FontAwesomeIcon icon={['far', 'copy']} />
+              </button>
+            </div>
             )}
           </div>
         </div>
